bcb23e3088158f607b7832ccbdb435dc64f93a57
ramjan-gh <ramzanhridoy@gmail.com>
2025-07-18 00:07:59 +0600

    stringify-parse

--- json.js
+++ json.js
@@ -1,2 +1,24 @@
 const user = { id: 1, name: "Gorib Amir", job: "actor" };
+const stringified = JSON.stringify(user);
 console.log(user);
+console.log(stringified);
+
+
+const shop = {
+    owner: 'Alia',
+    address: {
+        street: 'kochu khet',
+        city: 'Dhaka',
+        coutry: 'BD'
+    },
+    products: ['laptop', 'phone'],
+    revenue: 4500,
+    isOpen: true,
+    isNew: false
+}
+stringifiedShop = JSON.stringify(shop);
+console.log(shop);
+console.log(stringifiedShop);
+
+const shopObject = JSON.parse(stringifiedShop);
+console.log(shopObject)
